66edef95a09cfe99cddf78a9bfe40477045cf816
RaimondasTvaska <rtvaska@gmail.com>
2021-05-10 18:37:11 +0300

    Begins
    Pradzia

---
+++ script.js
@@ -0,0 +1,23 @@
+function markX(){
+    //When pressed on square must be marked as X
+}
+
+function oponentMarksO(){
+    // computer opponent makes a null
+}
+
+function getRandomMove(){
+    // analyses boards and calculates the next moves
+}
+
+function whoWon(){
+    // analyses the lines and calculates the winner
+}
+
+function isGameFinished(){
+
+}
+
+function resetGame(){
+    
+}
